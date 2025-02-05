a2cf103cc0a15614b1f75824562b8b8a889958b3
777Styx <143045695+777Styx@users.noreply.github.com>
2025-02-04 17:55:42 -0800

    Final

--- app/src/main/java/itson/edu/mx/guessthenumber/MainActivity.kt
+++ app/src/main/java/itson/edu/mx/guessthenumber/MainActivity.kt
@@ -1,13 +1,22 @@
 package itson.edu.mx.guessthenumber
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    var minValue = 0
+    var maxValue = 100
+    var num: Int = 0
+    var won = false
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -16,5 +25,68 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        //variables
+        val guessings: TextView = findViewById(R.id.txtGenerate)
+        val down: Button = findViewById(R.id.btnDown)
+        val up: Button = findViewById(R.id.btnUp)
+        val generate: Button = findViewById(R.id.btnGenerate)
+        val guessed: Button = findViewById(R.id.btnGuessed)
+
+        generate.setOnClickListener {
+            num = Random.nextInt(minValue, maxValue)
+            guessings.setText(num.toString())
+            generate.visibility = View.INVISIBLE
+            guessed.visibility = View.VISIBLE
+        }
+
+        up.setOnClickListener {
+            minValue = num
+            if (checkingLimits()) {
+                num = Random.nextInt(minValue, maxValue)
+                guessings.setText(num.toString())
+            } else {
+                guessings.setText("No puede ser; (me ganaste")
+            }
+        }
+
+        down.setOnClickListener {
+            maxValue = num
+            if (checkingLimits()) {
+                num = Random.nextInt(minValue, maxValue)
+                guessings.setText(num.toString())
+            } else {
+                guessings.setText(num.toString())
+            }
+        }
+
+        guessed.setOnClickListener {
+            if (!won) {
+                guessings.setText("Adivine tu numero es el " + num)
+                guessed.setText("Volveer a ")
+                won = true
+            } else {
+                generate.visibility = View.VISIBLE
+                generate.setText("Tap to generate to start")
+                guessed.visibility = View.GONE
+                resetValues()
+            }
+        }
+
+
     }
+
+    private fun checkingLimits(): Boolean {
+        return minValue != maxValue
+    }
+
+    fun resetValues() {
+        minValue = 0
+        maxValue = 100
+        num = 0
+        won = false
+    }
+
+
 }
